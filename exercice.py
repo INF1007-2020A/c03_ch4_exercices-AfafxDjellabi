7429f53f53cd302f2d1e678f2db0454b214a18e4
Afaf Djellabi <70678198+AfafxDjellabi@users.noreply.github.com>
2020-09-16 11:22:57 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,19 +3,14 @@
 
 
 def is_even_len(string: str) -> bool:
-    return False
+    return len(string) % 2 == 0
 
 
 def remove_third_char(string: str) -> str:
-    return ""
-
+    return string[:2] + string[3:]
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
-
-
-def get_nb_char(string: str, char: str) -> int:
-    return 0
+    return string.replace(old_char, new_char)
 
 
 def get_nb_words(sentence: str) -> int:
@@ -31,10 +26,11 @@ def main() -> None:
     print(f"On supprime le 3e caratère dans la chaine '{string}'. Résultat: {remove_third_char(string)}")
 
     string = "hello world!"
-    print(f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
+    print(
+        f"On remplace le caratère w par le caractère z dans la chaine: '{string}'. Résultat: {replace_char(string, 'w', 'z')}")
 
     print(f"Le nombre d'occurrence de l dans hello est : {get_nb_char(string, 'l')}")
-    
+
     string = "Baby shark doo doo doo doo doo doo"
     print(f"Le nombre de mots dans la chaine {string} est: {get_nb_words(string)}")
 
